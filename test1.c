faa39196dbd6a59cd0529d39b030b35b7a13e7c8
jermainealvarez048 <jermainealvarez048@gmail.com>
2025-07-07 00:03:34 +0800

    Trying some arrays

--- test1.c
+++ test1.c
@@ -2,12 +2,14 @@
 
 int main(void)
 {
-    int a[10], i;
+    int a[5] = {1, 2, 3, 4, 5};
+    int i;
 
-    for (i = 1; i <= 10; i++) {
-        a[i] = 1;
+    for (i = 0; i < 5; i++) {
+        a[i] = 0;
         printf("%d = %d\n", i, a[i]);
     }
 
+
     return 0;
-}   
+}
